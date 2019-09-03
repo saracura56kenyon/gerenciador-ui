82ee17be680365541dd8b0385ebd6875e8b5894e
saracura56Kenyon <wafjcf@gmail.com>
2019-09-03 09:40:58 -0400

    Deploy Mod

--- src/app/seguranca/auth.guard.ts
+++ src/app/seguranca/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
     if(this.auth.isAccessTokenInvalido()){
-
+      console.log('Navegação com access token inválido. Obtendo novo token...');
       return this.auth.obterNovoAccessToken()
         .then(() => {
           if(this.auth.isAccessTokenInvalido()) {
